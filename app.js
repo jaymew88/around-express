039ac5401e6e4e95ab5db30760039712f33a1d57
Jayme Waye <jaymewaye@gmail.com>
2020-10-17 16:02:15 -0400

    routes work

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const cards = require('./routes/cards');
+const users = require('./routes/users');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/cards', cards);
+app.use('/users', users);
+
+app.get('*',(req,res)=>{
+  return res.status(404).send({ "message": "Requested resource not found" });
+ });
+
+ app.listen(PORT, () => {
+  console.log(`App is listening on PORT ${PORT}`);
+});
---
+++ routes/cards.js
@@ -0,0 +1,16 @@
+const routes = require('express').Router();
+const fs = require('fs').promises;
+const path = require('path');
+const cardsPath = path.join(__dirname, '../data/cards.json');
+
+routes.get('/', (req, res) => {
+  fs.readFile(cardsPath, { encoding: 'utf-8' }).then((cards) => {
+    res.send(JSON.parse(cards));
+  })
+  .catch(() => {
+    res.status(500);
+    res.send({ message: 'Requested resource not found' });
+  })
+});
+
+module.exports = routes;
